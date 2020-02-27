093fccf36d60c53f91eca6f38b06f9e13bb2693d
Mulia Arifandi Nasution <mul14.net@gmail.com>
2020-02-27 15:14:05 +0700

    Initial commit

---
+++ precache-manifest.1b99af7751c44975ebfc4d6b424f609d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5030f92bf831393bf169dd8258216a17",
+    "url": "/demo-vue-kasir/index.html"
+  },
+  {
+    "revision": "65aca93e92a0dd27ddc4",
+    "url": "/demo-vue-kasir/js/about-legacy.9f0efb08.js"
+  },
+  {
+    "revision": "b4486347e4dfa8c0925c",
+    "url": "/demo-vue-kasir/js/app-legacy.f16ce41f.js"
+  },
+  {
+    "revision": "292c9a2255157a4d9431",
+    "url": "/demo-vue-kasir/js/chunk-2d0b60eb-legacy.91db44f7.js"
+  },
+  {
+    "revision": "17a3cac11dfb268a821e",
+    "url": "/demo-vue-kasir/js/chunk-2d20f8d6-legacy.1b2c5903.js"
+  },
+  {
+    "revision": "25833cdeb184b6b1d8df",
+    "url": "/demo-vue-kasir/js/chunk-2d21af69-legacy.d3b4734c.js"
+  },
+  {
+    "revision": "42658c22116e39940fc6",
+    "url": "/demo-vue-kasir/js/chunk-2d22ce84-legacy.da8f7f6b.js"
+  },
+  {
+    "revision": "c25eac29e5f0fb3b640f",
+    "url": "/demo-vue-kasir/js/chunk-vendors-legacy.b2ce02f4.js"
+  },
+  {
+    "revision": "49173196790f852a0070f45b25e83cf7",
+    "url": "/demo-vue-kasir/manifest.json"
+  }
+]);
---
+++ precache-manifest.259a42c752d88c5c4157dff73137b9a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243c04d5d7edbc9ab93373000cdaa240",
+    "url": "/demo-vue-kasir/index.html"
+  },
+  {
+    "revision": "958e8115d21d41591260",
+    "url": "/demo-vue-kasir/js/about.419c665e.js"
+  },
+  {
+    "revision": "43cd06f5b2c2b4d3252d",
+    "url": "/demo-vue-kasir/js/app.a90ec7fa.js"
+  },
+  {
+    "revision": "8fdf8ba16426327034eb",
+    "url": "/demo-vue-kasir/js/chunk-2d0b60eb.d5d8331c.js"
+  },
+  {
+    "revision": "d81ea25579ae837e5cbb",
+    "url": "/demo-vue-kasir/js/chunk-2d20f8d6.4c44babc.js"
+  },
+  {
+    "revision": "25833cdeb184b6b1d8df",
+    "url": "/demo-vue-kasir/js/chunk-2d21af69.d3b4734c.js"
+  },
+  {
+    "revision": "2f8dccc46253afe5f51a",
+    "url": "/demo-vue-kasir/js/chunk-2d22ce84.827a9552.js"
+  },
+  {
+    "revision": "d0ce74fc98a072406d31",
+    "url": "/demo-vue-kasir/js/chunk-vendors.7b8b2bd9.js"
+  },
+  {
+    "revision": "49173196790f852a0070f45b25e83cf7",
+    "url": "/demo-vue-kasir/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/demo-vue-kasir/robots.txt"
+  }
+]);
