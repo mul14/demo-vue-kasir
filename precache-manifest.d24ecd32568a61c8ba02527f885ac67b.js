edd80e19aa534ddd7c3dc2828d252e59485cd34f
Mulia Arifandi Nasution <mul14.net@gmail.com>
2020-05-18 21:13:50 +0700

    Netlify

---
+++ precache-manifest.d24ecd32568a61c8ba02527f885ac67b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb2cfa606755dd1093c1070e09dfc2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60c3f0c1251ef89c6f3f",
+    "url": "/js/about.483200f7.js"
+  },
+  {
+    "revision": "e4e4833a380d6a59a8f8",
+    "url": "/js/app.d62bfd7c.js"
+  },
+  {
+    "revision": "84446e7f034b085b34ef",
+    "url": "/js/chunk-2d0b60eb.ce91d17d.js"
+  },
+  {
+    "revision": "5c053e61dcf03684a187",
+    "url": "/js/chunk-2d20f8d6.6f0d0064.js"
+  },
+  {
+    "revision": "34f655239df6d2e123ba",
+    "url": "/js/chunk-2d21af69.fc15374a.js"
+  },
+  {
+    "revision": "42658c22116e39940fc6",
+    "url": "/js/chunk-2d22ce84.da8f7f6b.js"
+  },
+  {
+    "revision": "c25eac29e5f0fb3b640f",
+    "url": "/js/chunk-vendors.b2ce02f4.js"
+  },
+  {
+    "revision": "49173196790f852a0070f45b25e83cf7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
